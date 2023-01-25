270b469f8baad2aa9f1135440e9a377cde6b1795
James Hamilton <james.hamilton@guardsquare.com>
2023-01-26 00:35:28 +0100

    Add jloxloxlox and loxlox-nojar scripts

--- build.sh
+++ build.sh
@@ -1,9 +1,34 @@
 #/bin/sh
 
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "Java version 18+ required"
+    exit
+fi
 
-cd jlox/craftinginterpreters
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" < "18" ]]; then
+        echo "Java version 18+ required"
+        exit
+    fi
+fi
+
+(cd jlox/craftinginterpreters || exit
 make get
-cd ..
+pushd java/com/craftinginterpreters/lox || exit
+git apply ../../../../../../Interpreter.diff
+popd || exit
+make
+)
+(
+cd jlox || eixt
 ./gradlew copyJar
 bin/jlox ../Lox.lox ../lib/lox.jar
-cd ..
+)
--- test.sh
+++ test.sh
@@ -1,11 +1,21 @@
 #/bin/sh
 
-
-cd jlox/craftinginterpreters
-make get
-cd ..
-./gradlew copyJar
+(
+echo "Testing Lox.lox with original jlox interpreter"
+cd jlox || exit
+cd craftinginterpreters || exit
+dart tool/bin/test.dart jlox
+)
+(
+echo "Testing Lox.lox with jlox compiler"
+cd jlox || exit
+cd craftinginterpreters || exit
+dart tool/bin/test.dart jlox -i ../../bin/loxlox-nojar
+)
+(
+echo "Testing Lox.lox with jlox compiler compiled jar"
+cd jlox || exit
 bin/jlox ../Lox.lox ../lib/lox.jar
-cd craftinginterpreters
+cd craftinginterpreters || exit
 dart tool/bin/test.dart jlox -i ../../bin/loxlox
-
+)
