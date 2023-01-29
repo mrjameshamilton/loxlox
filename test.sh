08506fbe57d3740a86fda3ce70608266ab77c9f6
James Hamilton <mrjameshamilton@gmail.com>
2023-01-29 09:52:31 +0100

    Remove testing with non-jar jlox

--- test.sh
+++ test.sh
@@ -7,12 +7,6 @@ cd craftinginterpreters || exit
 dart tool/bin/test.dart jlox -i ../../bin/jloxloxlox-original
 )
 (
-echo "Testing Lox.lox with jlox compiler"
-cd jlox || exit
-cd craftinginterpreters || exit
-dart tool/bin/test.dart jlox -i ../../bin/loxlox-nojar
-)
-(
 echo "Testing Lox.lox with jlox compiler compiled jar"
 cd jlox || exit
 bin/jlox ../Lox.lox ../lib/lox.jar
